6a7156d91e98f0c0d0974ce4c853f3f175afcc84
faizan542 <faizansilat100@gmail.com>
2022-10-07 21:11:41 +0500

    Registration Form

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+// import Login from './component/Login';
+// import Form from './component/Form';
+
+// let isLoggedIn = false;
+
+// let userIsRegistered = false;
+
+
+function App() {
+  return (
+    <div className="container">
+      {/* {isLoggedIn === true ? <h1>Hello</h1> : <Login />} */}
+      {/* <Form isRegistered={userIsRegistered}/> */}
+    </div>
+  );
+}
+
+export default App;
